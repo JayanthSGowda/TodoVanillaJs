cb7fa6a07d05361be766cd8e2d9ce1e77e9eb0e9
luffy <jayanth.gowda2807@gmail.com>
2023-07-15 13:44:54 +0530

    added render add and delete functions and some images

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ to do list app
 
 
 getting started with the skill test 
+added some basic html and styles to it
+
+since the structure is complete/ ready getting to the functionns
+like
+add task 
+delete task 
+marking task as complete 
+rendering the task list
---
+++ index.js
@@ -0,0 +1,74 @@
+let tasks = [];
+const taskList = document.getElementById('list');
+const addTaskInput = document.getElementById('add');
+const tasksCounter = document.getElementById('tasks-counter');
+
+function addTaskToDOM(task){
+    const li = document.createElement('li');
+    li.innerHTML = `
+        <input type="checkbox" id="${task.id}" ${task.done ? 'checked': ''} class="custom-checkbox">
+        <label for="${task.id}"">${task.text}</label>
+        <img src="/images/bin.png" alt="del" class="delete" data-id="${task.id}"">
+
+    `;
+
+    taskList.append(li);
+}
+
+function renderList(){
+    taskList.innerHTML = '';
+    tasks.forEach(addTaskToDOM);
+    tasksCounter.innerHTML = tasks.length;
+}
+
+function toggleTask(taskId){
+    const task = tasks.filter((task) => task.id === taskId);
+
+    if(task.length > 0){
+        const currentTask = task[0];
+        currentTask.document = !currentTask.done;
+        renderList();
+    }
+    showNotification('something went wrong');
+}
+
+function deleteTask(taskId){
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    tasks = newTasks;
+    renderList();
+    showNotification('task deleted successfully');
+}
+
+function addTask(task){
+    if(task){
+        tasks.push(task);
+        renderList();
+        showNotification('task added successfully');
+        return;
+    }
+    showNotification('task cannot be added');
+}
+
+function showNotification(text){
+    alert(text);
+}
+
+function handleInputKeypress(e){
+    if(e.key === 'Enter'){
+        const text = e.target.value;
+        
+        if(!text){
+            showNotification('Task text cannot be empty');
+            return;
+        }
+
+        const task = {
+            text,
+            id: Date.now().toString(),
+            done: false
+        }
+        e.target.value = '';
+        addTask(task);
+    }
+}
+addTaskInput.addEventListener('keyup', handleInputKeypress);
